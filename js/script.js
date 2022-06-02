431eae751b8ed8ad3893a9b14e182f69bb1525b6
zenizee563 <zeenat.parveen@thedevnation.com>
2022-06-02 17:19:48 +0500

    emailing issue fix with two way communication

--- js/script.js
+++ js/script.js
@@ -5,6 +5,11 @@ const form=document.querySelector('form')
 const txtusername=document.querySelector('.username')
 const txtemail=document.querySelector('.useremail')
 const txtmsg=document.querySelector('.usermsg')
+const recaptcha=document.querySelector('.g-recaptcha')
+const detailscheck=document.querySelector('#detailscheck')
+detailscheck.addEventListener('change',()=>{
+    console.log(detailscheck.checked)
+})
 
 btnGetInTouch.addEventListener('click',()=>{
 
@@ -21,12 +26,14 @@ var openGetInTouch=()=>{
 btncancel.addEventListener('click', (e)=>{
     e.preventDefault()
     form.reset()
+    grecaptcha.reset()
     formcontainer.classList.add('d-none')
    
 })
 
 form.addEventListener('submit',(e)=>{
     e.preventDefault();
+    
     if(txtusername.value.trim()===""){
         alert("Please Fill All GetInTouch Input Fields")
         return;
@@ -35,37 +42,62 @@ form.addEventListener('submit',(e)=>{
         alert("Please Fill All GetInTouch Input Fields")
         return;
     }
-    emailsend();
+    if(!detailscheck.checked){
+        alert("Please check this agrement policy ")
+        return;
+    }
+    if(grecaptcha.getResponse().length===0){
+        alert("Please Fill recaptcha to confirm you are not robot")
+        return;
+    }
+   
+    grecaptcha.reset()
+    sendMail();
     form.reset();    
     formcontainer.classList.add('d-none')
-    setTimeout(function(){ alert("Your Email Sent Successfully. You Will Get Email Confirmation."); }, 2000);
 })
 
 
-const  emailsend=()=>{
-    Email.send({
+// const  emailsend=()=>{
+//     Email.send({
         
-        SecureToken : "caba590a-d790-4496-ba6d-591b7b4e3d90",
-        To : txtemail.value.trim(),
-        From : "info@tech-iomes.io",
-        Subject : "GET IN TOUCH (IOMES)",
-        Body : `Hello <b> ${txtusername.value.trim()}</b>: <br> Thank you for contacting us.<br>We have recieved your email.<br>We will reply you very soon.<br><br> <b>Best Regards:</b><br><span>IOMES Group</span>`
-    }).then(
+//         SecureToken : "69e174d2-2236-4e41-a2eb-1a9b63966628",
+//         To : txtemail.value.trim(),
+//         From : "info@tech-iomes.io",
+//         Subject : "GET IN TOUCH (IOMES)",
+//         Body : `Hello <b> ${txtusername.value.trim()}</b>: <br> Thank you for contacting us.<br>We have recieved your email.<br>We will reply you very soon.<br><br> <b>Best Regards:</b><br><span>IOMES Group</span>`
+//     }).then(res=>{console.log("aaaa",res)}
     
-    ).catch(err=>{
-        console.log(err)
-    })
-    Email.send({
-        SecureToken : "caba590a-d790-4496-ba6d-591b7b4e3d90",
-        To : 'info@tech-iomes.io',
-        From : "info@tech-iomes.io",
-        Subject : `GET IN TOUCH (IOMES)`,
-        Body : `<b>${txtusername.value}</b> is trying to connect you through Get In Touch from this <b>Email: ${txtemail.value.trim()}</b> <br><br> ${txtmsg.value.trim()}  `
-    }).then(
+//     ).catch(err=>{
+//         console.log(err)
+//     })
+//     Email.send({
+//         SecureToken : "69e174d2-2236-4e41-a2eb-1a9b63966628",
+//         To : 'info@tech-iomes.io',
+//         From : "info@tech-iomes.io",
+//         Subject : `GET IN TOUCH (IOMES)`,
+//         Body : `<b>${txtusername.value}</b> is trying to connect you through Get In Touch from this <b>Email: ${txtemail.value.trim()}</b> <br><br> ${txtmsg.value.trim()}  `
+//     }).then(res=>{console.log("bbb",res)}
         
-    ).catch(err=>{
-        console.log(err)
-    })
+        
+//     ).catch(err=>{
+//         console.log(err)
+//     })
+// }
+const sendMail=()=>{
+    var tempParams={
+        from_name:txtusername.value.trim(),
+        from_email:txtemail.value.trim(),        
+        message:txtmsg.value.trim()
+        }
+        emailjs.send('service_c9a1imi','template_6oryc6g',tempParams).then(res=>{        
+            emailjs.send('service_c9a1imi','template_31g81l9',tempParams).then(resp=>{
+                alert("Your email send to IOMES Group Limited . you will recieve confirmation email to yor inbox soon. Thankyou")
+            })
+        })
+        .catch(err=>{
+            console.log("Failed",err.message)
+        })
 }
 
           var lessevents=document.querySelector('.less')
